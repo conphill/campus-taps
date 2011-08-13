00275a95317eaefda2160e4075fb6a3023afc664
sphillips <phillips.sb@gmail.com>
2011-08-12 20:04:53 -0400

    fixed migration

--- db/migrate/20110807235300_add_indices_for_state.rb
+++ db/migrate/20110807235300_add_indices_for_state.rb
@@ -2,12 +2,10 @@ class AddIndicesForState < ActiveRecord::Migration
   def self.up
     add_index :bars, :state_id
     add_index :stores, :state_id
-    add_index :regions, :state_id
   end
 
   def self.down
     remove_index :bars, :state_id
     remove_index :stores, :state_id
-    remove_index :regions, :state_id
   end
 end
