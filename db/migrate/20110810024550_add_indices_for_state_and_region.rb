6b539af64f12a558f4bf356a6b2ffe0f943e31e9
sphillips <phillips.sb@gmail.com>
2011-08-12 20:03:12 -0400

    commented out migration

--- db/migrate/20110810024550_add_indices_for_state_and_region.rb
+++ db/migrate/20110810024550_add_indices_for_state_and_region.rb
@@ -1,11 +1,11 @@
-class AddIndicesForStateAndRegion < ActiveRecord::Migration
-  def self.up
-    add_index :regions, :state_id
-    add_index :restaurants, :region_id
-  end
-
-  def self.down
-    remove_index :regions, :state_id
-    remove_index :restaurants, :region_id
-  end
-end
+# class AddIndicesForStateAndRegion < ActiveRecord::Migration
+#   def self.up
+#     add_index :regions, :state_id
+#     add_index :restaurants, :region_id
+#   end
+# 
+#   def self.down
+#     remove_index :regions, :state_id
+#     remove_index :restaurants, :region_id
+#   end
+# end
