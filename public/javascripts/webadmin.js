9fe95ee5aea465f23025ca7324c583cc8cb8ed4b
sphillips <sphillips@blue-iceberg.com>
2011-10-13 17:05:13 -0400

    tweaking tinymce settings

--- public/javascripts/webadmin.js
+++ public/javascripts/webadmin.js
@@ -11,7 +11,8 @@ $(document).ready(function(){
 		theme_advanced_toolbar_location : "top",
 		theme_advanced_toolbar_align : "left",
 		theme_advanced_statusbar_location : "bottom",
-		theme_advanced_resizing : true,
+		// theme_advanced_blockformats:"p,h3",
+		// theme_advanced_resizing : false,
 		// Example content CSS (should be your site CSS)
 		content_css : "/stylesheets/style.css",
 		// Drop lists for link/image/media/template dialogs
